4492f8355b6493144398f0a0e2730c814f056adb
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-12-21 15:12:45 +0100

    Exercice 1

--- JAVA/TD_1/Exercice_2.java
+++ JAVA/TD_1/Exercice_2.java
@@ -1,6 +1,8 @@
-public class TD_1 {
+package TD_1;
+
+public class Exercice_2 {
     public static void main(String[] args) {
-double score = 92.7;
+double score = 84.7;
 int intScore = (int) score;
 int category = intScore / 10;
 char grade;
@@ -25,16 +27,3 @@ switch (category) {
 System.out.println("La note est : " + grade);
     }
 }
-// Un jeu vidéo stocke le score du joueur dans un double. On veut convertir ce score en une 
-// note (char) basée sur des paliers entiers.
-// • Déclarez double score = 84.7;
-// • Convertissez ce score en int (ce qui va le tronquer, c'est voulu).
-// • Divisez cet int par 10 pour obtenir une "catégorie" (un int de 0 à 10).
-// • Utilisez un switch sur cette catégorie pour affecter une note à une variable char 
-// grade ainsi:
-// ◦ 10 et 9 : 'A'
-// ◦ 8 : 'B'
-// ◦ 7 : 'C'
-// ◦ 6 : 'D'
-// ◦ Autre (default) : 'F'
-// • Affichez la note (grade).
